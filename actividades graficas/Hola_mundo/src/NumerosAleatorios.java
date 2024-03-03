9f656905ebf64892c7ca23f95c8403914dd46cfc
ocurrentduke1 <107155647+ocurrentduke1@users.noreply.github.com>
2024-03-03 12:01:47 -0600

    carpeta out

---
+++ actividades graficas/Hola_mundo/src/NumerosAleatorios.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int num1 = random.nextInt(100);
+        int num2 = random.nextInt(100);
+
+        System.out.println("los numeros aleatorios son: \n" + num1 +"\n" + num2);
+
+        if(num1 < num2){
+            System.out.println("el numero mayor es: " + num2);
+        } else {
+            System.out.println("el numero mayor es: " + num1);
+        }
+    }
+}
---
+++ actividades graficas/Hola_mundo/src/visorImagen.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class visorImagen extends JFrame {
+    private JScrollPane panel;
+    private Pantalla pantalla;
+
+    public visorImagen(){
+        super("visor imagen");
+        Image img = Toolkit.getDefaultToolkit().getImage("C:\\Users\\vdavi\\Desktop\\actividades graficas\\Hola_mundo\\imagenes\\arbol.jpg");
+        pantalla = new Pantalla();
+        panel = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        getContentPane().add(panel);
+        panel.setViewportView(pantalla);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+         new visorImagen();
+    }
+}
